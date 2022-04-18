958f63851ac848ba728d41d1bf9aaf37751c44d7
Josu Martin <josu.martin.sanchez@gmail.com>
2022-04-18 18:57:05 +0200

    Iteracciones obligaciones hechas

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,66 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(healthParam, strengthParam) {
+    this.health = healthParam;
+    this.strength = strengthParam;
+  }
+  attack = () => {
+    return this.strength;
+  };
+  receiveDamage = (damageParameter) => {
+    this.health -= damageParameter;
+  };
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(nameParam, healthParam, strengthParam) {
+    super(healthParam, strengthParam);
+    this.name = nameParam;
+  }
+  receiveDamage = (damageParameter) => {
+    this.health = this.health - damageParameter;
+    if (this.health > 0) {
+      return `${this.name} has received ${damageParameter} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  };
+  battleCry = () => {
+    return 'Odin Owns You All!';
+  };
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage = (damageParameter) => {
+    this.health = this.health - damageParameter;
+    if (this.health > 0) {
+      return `A Saxon has received ${damageParameter} points of damage`;
+    } else {
+      return 'A Saxon has died in combat';
+    }
+  };
+}
 
 // War
-class War {}
 
-
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking = (viking) => {
+    this.vikingArmy.push(viking);
+  };
+  addSaxon = (saxon) => {
+    this.saxonArmy.push(saxon);
+  };
+  vikingAttack = () => {
+    Map.random(this.saxonArmy) * this.saxonArmy.length;
+    Map.random(this.vikingArmy) * this.vikingArmy.length;
+  };
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
